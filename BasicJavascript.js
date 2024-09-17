005729bdc8ee47ca1eaba249fb87a16f6eabcf23
Imtiaz <134070730+amimtiaz@users.noreply.github.com>
2024-09-17 17:26:17 +0600

    Create BasicJavascript.js

---
+++ BasicJavascript.js
@@ -0,0 +1,16 @@
+// Declaring variables
+let name = 'John'; // String
+let age = 30; // Number
+let isActive = true; // Boolean
+
+// Array
+let fruits = ['Apple', 'Banana', 'Orange'];
+
+// Object
+let person = {
+    name: 'John',
+    age: 30,
+    isActive: true
+};
+
+console.log(name, age, isActive, fruits, person);
